5bb7163c262cae0138a45f9705d22dc3e67e0450
pichugina-v <Psyde26@yandex.ru>
2023-02-07 13:10:03 +0100

    fixed: xslx style

--- app/v1/celery/tasks.py
+++ app/v1/celery/tasks.py
@@ -7,31 +7,33 @@ from .celery import celery
 def form_excel(extracted_data):
     workbook = xlwt.Workbook()
     sheet = workbook.add_sheet('Menus')
+    style = xlwt.easyxf('font: bold on')
 
-    idx = 0
-    num1 = 1
+    row: int = 0
+    menu_serial_num: int = 1
+
     for data in extracted_data:
-        sheet.write(idx, 0, num1)
-        sheet.write(idx, 1, data['title'])
-        sheet.write(idx, 2, data['description'])
-        idx += 1
-        num2 = 1
+        sheet.write(row, 0, menu_serial_num, style)
+        sheet.write(row, 1, data['title'], style)
+        sheet.write(row, 2, data['description'], style)
+        row += 1
 
+        submenu_serial_num: int = 1
         for submenu in data['submenus']:
-            sheet.write(idx, 1, num2)
-            sheet.write(idx, 2, submenu['title'])
-            sheet.write(idx, 3, submenu['description'])
-            num2 += 1
-            idx += 1
-            num3 = 1
+            sheet.write(row, 1, submenu_serial_num, style)
+            sheet.write(row, 2, submenu['title'], style)
+            sheet.write(row, 3, submenu['description'], style)
+            submenu_serial_num += 1
+            row += 1
 
+            dish_serial_num: int = 1
             for dish in submenu['dishes']:
-                sheet.write(idx, 2, num3)
-                sheet.write(idx, 3, dish['title'])
-                sheet.write(idx, 4, dish['description'])
-                sheet.write(idx, 5, dish['price'])
-                num3 += 1
-                idx += 1
+                sheet.write(row, 2, dish_serial_num, style)
+                sheet.write(row, 3, dish['title'], style)
+                sheet.write(row, 4, dish['description'], style)
+                sheet.write(row, 5, dish['price'], style)
+                dish_serial_num += 1
+                row += 1
 
     task_id = celery.current_task.request.id
     workbook.save(f'./reports/{task_id}.xlsx')
